0c510e5bae1e3524b8c5e5503a4bbdc6f55f5043
Zixia Lu <zixialu@gmail.com>
2018-12-20 12:09:19 -0500

    WIP implement POST new poll route and install uuid

--- routes/new-poll.js
+++ routes/new-poll.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const uuidv4 = require('uuid/v4');
+
 const express = require('express');
 const router = express.Router();
 
@@ -8,7 +10,25 @@ module.exports = knex => {
   router.get('/', (req, res) => {});
 
   // POST create new poll
-  router.post('/', (req, res) => {});
+  router.post('/', (req, res) => {
+    // TODO: Generate/pull all poll parameters out from the request as follows:
+    // Poller
+    const pollerEmail = req.body.email;
+
+    // Poll
+    const prompt = req.body.prompt;
+    const createDate = Date();
+    // TODO: Stretch: set a close date if applicable
+    const closeDate = null;
+    const publicId = uuidv4();
+
+    /*
+     * FIXME: Refactor pollers table in db to be primary keyed by email, so we
+     * don't have to do a lookup to find matching id or have multiple bogus entries
+     */
+    // TODO: Insert new poller, then poll, then choices
+    // TODO: Refactor helper functions out to a data-helpers file
+  });
 
   return router;
 };
