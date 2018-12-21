bb0c78183cbeaaca373f28da9af2d184eeebcf5a
Zixia Lu <zixialu@gmail.com>
2018-12-20 20:05:05 -0500

    Implement jQueryUI sortable list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,23 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: 'GET',
+  //   url: '/api/users'
+  // }).done(users => {
+  //   for (user of users) {
+  //     $('<div>')
+  //       .text(user.name)
+  //       .appendTo($('body'));
+  //   }
+  // });
+
+  // Drag and drop list
+  $('#sortable').sortable({
+    // axis: 'y',
+    containment: 'parent',
+    cursor: 'move',
+    distance: 5,
+    revert: true,
+    tolerance: 'pointer'
+  });
+  // $('#sortable').disableSelection();
 });
