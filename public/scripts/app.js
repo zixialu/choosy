dcc11137453164c5219cc8f1e3ead47327f8cad7
Zixia Lu <zixialu@gmail.com>
2018-12-21 10:03:25 -0500

    Move sortable list logic from app.js to move.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,17 @@
 $(document).ready(function() {
-
   //Ajax POST request on form submit on main page.
-  let $newpoll = $("#create-poll"); //important: confirm id against html file
-  $newpoll.submit(function (event) {
+  let $newpoll = $('#create-poll'); //important: confirm id against html file
+  $newpoll.submit(function(event) {
     console.log('Button clicked!');
     event.preventDefault();
     //TO DO: create helper function to validate submission
     let formData = $newpoll.serialize();
-    $.post('/', formData, function (data, status) {
+    $.post('/', formData, function(data, status) {
       //TO DO: consider if we call a helper function here that automatically
       //redirects to manage page automatically (or a "success" page).
     });
-  })
-
-  // Drag and drop list
-  $('#sortable').sortable({
-    // axis: 'y',
-    containment: 'parent',
-    cursor: 'move',
-    distance: 5,
-    // This blocks arranging while it drifts; remove for UX reasons?
-    // revert: true,
-    // This can get buggy if elements are different sizes
-    tolerance: 'pointer'
   });
-  $('#sortable').disableSelection();
-})
+});
 
 // $(() => {
 //   $.ajax({
@@ -37,4 +23,3 @@ $(document).ready(function() {
 //     }
 //   });;
 // });
-
--- public/scripts/vote.js
+++ public/scripts/vote.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-
   //Ajax POST request on form submit on vote page.
-  let $votepoll = $("#vote-button");
-  $votepoll.submit(function (event) {
+  let $votepoll = $('#vote-button');
+  $votepoll.submit(function(event) {
     console.log('Button clicked!');
     event.preventDefault();
     let voteFormData = $votepoll.serialize();
+  });
 
-  })
-
-})
+  // Drag and drop list
+  $('#sortable').sortable({
+    // axis: 'y',
+    containment: 'parent',
+    cursor: 'move',
+    distance: 5,
+    // This blocks arranging while it drifts; remove for UX reasons?
+    // revert: true,
+    // This can get buggy if elements are different sizes
+    tolerance: 'pointer'
+  });
+  $('#sortable').disableSelection();
+});
