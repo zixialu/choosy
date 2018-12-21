c991178020e9d2f740304c6ca6a22227d3eec2d9
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-21 14:36:07 +0000

    Setting up and writing jQuery files.

---
+++ public/scripts/vote.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  //Ajax POST request on form submit on vote page.
+  let $votepoll = $("#vote-button");
+  $votepoll.submit(function (event) {
+    console.log('Button clicked!');
+    event.preventDefault();
+    let voteFormData = $votepoll.serialize();
+
+  })
+
+})
