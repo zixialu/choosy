6b5cfa74c6eaa4ca23533dfe1e85aa1e314ce20f
Zixia Lu <zixialu@gmail.com>
2018-12-22 11:44:53 -0500

    Prettier app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,23 @@
 $(function() {
-
   //Ajax POST request on form submit on main page.
   let $newpoll = $('#create-poll'); //important: confirm id against html file
-  $newpoll.submit(function (event) {
+  $newpoll.submit(function(event) {
     console.log('Button clicked!');
     event.preventDefault();
     //TO DO: create helper function to validate submission
     let formData = $newpoll.serialize();
 
-    $.post('/', formData, function (data, status) {
+    $.post('/', formData, function(data, status) {
       // console.log("data that comes back from post request", data);
-    //   let pollId = ***;
-    //   //TO DO: figure out how to get pollID from data sent back by POST req
-    //   //TO DO: figure out how to hash pollID
-    //   res.redirect('/manage/:pollId');
+      //   let pollId = ***;
+      //   //TO DO: figure out how to get pollID from data sent back by POST req
+      //   //TO DO: figure out how to hash pollID
+      //   res.redirect('/manage/:pollId');
     });
-  })
+  });
 
   console.log('Test');
-})
-
-
-
-
-
-//   // Drag and drop list
-//   $('#sortable').sortable({
-//     // axis: 'y',
-//     containment: 'parent',
-//     cursor: 'move',
-//     distance: 5,
-//     // This blocks arranging while it drifts; remove for UX reasons?
-//     // revert: true,
-//     // This can get buggy if elements are different sizes
-//     tolerance: 'pointer'
-//   });
-//   $('#sortable').disableSelection();
-// })
+});
 
 // $(() => {
 //   $.ajax({
@@ -47,4 +28,3 @@ $(function() {
 //       $("<div>").text(user.name).appendTo($("body"));
 //     }
 //   });;
-
