b275c42b81b9ac138494d9c32467e0fe20be00c6
Zixia Lu <zixialu@gmail.com>
2018-12-27 18:28:56 -0500

    Style chart colour and y axis

--- public/scripts/manage.js
+++ public/scripts/manage.js
@@ -47,22 +47,8 @@ function createChart(data) {
         {
           label: 'Percentage',
           data: rankPercentages,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255,99,132,1)',
           borderWidth: 1
         }
       ]
@@ -77,6 +63,15 @@ function createChart(data) {
               callback: function(value) {
                 return value + '%';
               }
+            }
+          }
+        ],
+        yAxes: [
+          {
+            // This constrains bar width
+            maxBarThickness: 80,
+            gridLines: {
+              display: false
             }
           }
         ]
