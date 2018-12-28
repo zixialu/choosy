5cfda67ac58eb83901bcf6a51a3d6fb1abfe7000
Zixia Lu <zixialu@gmail.com>
2018-12-28 15:05:44 -0500

    Fix issue where json was passed as a key to an empty value

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 $(function() {
   //Ajax POST request on form submit on main page.
   let $pollForm = $('#create-poll');
-  let $choices = $('#poll-choices')
+  let $choices = $('#poll-choices');
 
   // Submit form event
   $pollForm.submit(function(event) {
@@ -9,40 +9,44 @@ $(function() {
     event.preventDefault();
     // TODO: create helper function to validate submission
 
-
     // let formData = $pollForm.serialize();
 
     let formData = jQuery($pollForm).serializeArray();
     let parsedFormData = {};
-    console.log("input", formData)
+    console.log('input', formData);
     formValidation(formData);
-
 
     function formValidation(data) {
       if (!data[0].value) {
-        $('#form-alert .panel-body').text('Please write a prompt for your poll')
+        $('#form-alert .panel-body').text(
+          'Please write a prompt for your poll'
+        );
+        console.log('Please write a prompt for your poll');
       } else if (!data[1].value) {
-        $('form-alert .panel-body').text('Please insert a valid email address')
+        $('form-alert .panel-body').text('Please insert a valid email address');
+        console.log('Please insert a valid email address');
       } else {
-        parsedFormData[data[0].name] = data[0].value
-        parsedFormData[data[1].name] = data[1].value
+        parsedFormData[data[0].name] = data[0].value;
+        parsedFormData[data[1].name] = data[1].value;
         for (let counter = 2; counter < data.length; counter += 2) {
           if (data[counter].value) {
             parsedFormData[data[counter].name] = data[counter].value;
-            parsedFormData[data[counter + 1].name] = data[counter + 1].value
+            parsedFormData[data[counter + 1].name] = data[counter + 1].value;
           }
         }
         if (parsedFormData.length <= 4) {
-          $('#form-alert .panel-body').text('Please create a poll with at least two options')
+          $('#form-alert .panel-body').text(
+            'Please create a poll with at least two options'
+          );
         } else {
-          console.log('parsed data', parsedFormData)
-          let newData = JSON.stringify(parsedFormData)
-          $.post('/', newData, function(data, status) {
-      // console.log(data)// console.log("data that comes back from post request", data);
-      //   let pollId = ***;
-      //   // TODO: figure out how to get pollID from data sent back by POST req
-      //   // TODO: figure out how to hash pollID
-      //   res.redirect('/manage/:pollId');
+          console.log('parsed data', parsedFormData);
+          let newData = JSON.stringify(parsedFormData);
+          $.post('/', parsedFormData, function(data, status) {
+            // console.log(data)// console.log("data that comes back from post request", data);
+            //   let pollId = ***;
+            //   // TODO: figure out how to get pollID from data sent back by POST req
+            //   // TODO: figure out how to hash pollID
+            //   res.redirect('/manage/:pollId');
           });
         }
       }
@@ -99,10 +103,7 @@ function createChoiceGroup(number) {
       />
     </div>
   `;
-
 }
-
-
 
 // $(() => {
 //   $.ajax({
